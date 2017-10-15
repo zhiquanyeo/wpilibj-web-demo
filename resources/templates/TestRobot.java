82c7c6187db71d9c315f623afec00354e025b28b
Zhiquan Yeo <zhiquan.yeo@gmail.com>
2017-10-15 14:02:48 -0400

    Fix bug in sample code

--- resources/templates/TestRobot.java
+++ resources/templates/TestRobot.java
@@ -6,22 +6,22 @@ public class TestRobot extends SampleRobot {
     // The RobotDrive is used to control the motors on the robot.
     private RobotDrive drivetrain = new RobotDrive(0, 1);
 
-    // TODO Try driving forward (negative speed), and see which way the wheels
-    // turn. if the left wheel is turning backwards, uncomment the line with
-    // 'kRearLeft'. If the right wheel is turning backwards, uncomment the line
-    // with 'kRearRight'. If BOTH wheels are turning backwards, uncomment both 
-    // lines. 
-    //
-    // NOTE: You can also switch the wires around in lieu of uncommenting the
-    // respective code lines
-    
-    // drivetrain.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
-    // drivetrain.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
-
     // This function gets called once when the robot wakes up
     protected void robotInit() {
         // Here, we print a welcome message
         System.out.println("Hello! I am TestRobot");
+
+        // TODO Try driving forward (negative speed), and see which way the wheels
+        // turn. if the left wheel is turning backwards, uncomment the line with
+        // 'kRearLeft'. If the right wheel is turning backwards, uncomment the line
+        // with 'kRearRight'. If BOTH wheels are turning backwards, uncomment both 
+        // lines. 
+        //
+        // NOTE: You can also switch the wires around in lieu of uncommenting the
+        // respective code lines
+        
+        // drivetrain.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
+        // drivetrain.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
     }
 
     // TThe code in this function gets run when the robot enters autonomous mode
